c0e908bf753d7174e49610b79e587b14e8583c41
Nathan Novacek <novacekn@gmail.com>
2021-01-16 22:37:11 -0500

    Added in some tests.

--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -1,8 +1,11 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
@@ -32,10 +35,68 @@ class CloudStorageApplicationTests {
 		}
 	}
 
+	public void createNewUser() {
+		driver.get("http://localhost:" + this.port + "/signup");
+		WebElement firstName = driver.findElement(By.name("firstName"));
+		WebElement lastName = driver.findElement(By.name("lastName"));
+		WebElement username = driver.findElement(By.name("username"));
+		WebElement password = driver.findElement(By.name("password"));
+		WebElement submitButton = driver.findElement(By.id("submit-button"));
+
+		firstName.sendKeys("John");
+		lastName.sendKeys("Doe");
+		username.sendKeys("jdoe");
+		password.sendKeys("password");
+		submitButton.submit();
+	}
+
+	public void createNote() {
+
+	}
+
+	public void loginUser() {
+		driver.get("http://localhost:" + this.port + "/login");
+		WebElement username = driver.findElement(By.name("username"));
+		WebElement password = driver.findElement(By.name("password"));
+		WebElement submitButton = driver.findElement(By.id("submit-button"));
+		username.sendKeys("jdoe");
+		password.sendKeys("password");
+		submitButton.submit();
+	}
+
 	@Test
-	public void getLoginPage() {
+	public void testUnauthorizedUserCannotAccessHomePage() {
+		driver.get("http://localhost:" + this.port + "/home");
+		Assertions.assertEquals("Login", driver.getTitle());
+	}
+
+	@Test
+	public void testUnauthorizedUserCanAccessLoginPage() {
 		driver.get("http://localhost:" + this.port + "/login");
 		Assertions.assertEquals("Login", driver.getTitle());
 	}
 
+	@Test
+	public void testUnauthorizedUserCanAccessSignupPage() {
+		driver.get("http://localhost:" + this.port + "/signup");
+		Assertions.assertEquals("Sign Up", driver.getTitle());
+	}
+
+	@Test
+	public void testAuthorizedUserCanAccessHomePage() {
+		createNewUser();
+		loginUser();
+		Assertions.assertEquals("Home", driver.getTitle());
+	}
+
+	@Test
+	public void testLogoutUser() {
+		createNewUser();
+		loginUser();
+		WebElement logoutButton = driver.findElement(By.id("logout-button"));
+		logoutButton.submit();
+		Assertions.assertEquals("Login", driver.getTitle());
+		driver.get("http://localhost:" + this.port + "/home");
+		Assertions.assertEquals("Login", driver.getTitle());
+	}
 }
