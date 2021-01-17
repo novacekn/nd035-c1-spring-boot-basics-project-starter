5d8daa199bdc87e4b3770b8f5435c91eabe63fef
Nathan Novacek <novacekn@gmail.com>
2021-01-17 10:38:45 -0500

    Working on automated testing.

--- starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/CloudStorageApplicationTests.java
@@ -1,16 +1,24 @@
 package com.udacity.jwdnd.course1.cloudstorage;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+
+/*
+	Was having an issue with the H2 in memory database not clearing data after each test. Found the decorator
+	solutions below at this link https://stackoverflow.com/questions/34617152/how-to-re-create-database-before-each-test-in-spring
+ */
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class CloudStorageApplicationTests {
 
 	@LocalServerPort
@@ -50,8 +58,74 @@ class CloudStorageApplicationTests {
 		submitButton.submit();
 	}
 
-	public void createNote() {
+	public void createNote() throws InterruptedException {
+		createNewUser();
+		loginUser();
+		Thread.sleep(1000);
+		WebElement navNotesTab = driver.findElement(By.id("nav-notes-tab"));
+		navNotesTab.click();
+		Thread.sleep(1000);
+		WebElement addNoteButton = driver.findElement(By.id("add-note-button"));
+		addNoteButton.click();
+		Thread.sleep(1000);
+		WebElement noteTitle = driver.findElement(By.name("noteTitle"));
+		WebElement noteDescription = driver.findElement(By.name("noteDescription"));
+		WebElement noteSubmit = driver.findElement(By.id("noteSubmit"));
+		noteTitle.sendKeys("Title 1");
+		noteDescription.sendKeys("Description 1");
+		noteSubmit.submit();
+	}
 
+	@Test
+	public void testNoteCreation() throws InterruptedException {
+		createNote();
+		Assertions.assertEquals("Result", driver.getTitle());
+		WebElement successMessage = driver.findElement(By.id("success-msg"));
+		Assertions.assertEquals("Your note has been successfully created.", successMessage.getText());
+	}
+
+	@Test
+	public void testCreatedNoteIsDisplayed() throws InterruptedException {
+		createNote();
+		driver.get("http://localhost:" + this.port + "/home");
+		WebElement navNotesTab = driver.findElement(By.id("nav-notes-tab"));
+		navNotesTab.click();
+		Thread.sleep(1000);
+		WebElement noteTitle1 = driver.findElement(By.id("noteTitle1"));
+		WebElement noteDescription1 = driver.findElement(By.id("noteDescription1"));
+		Assertions.assertEquals("Title 1", noteTitle1.getText());
+		Assertions.assertEquals("Description 1", noteDescription1.getText());
+	}
+
+	@Test
+	public void testEditNote() throws InterruptedException {
+		createNote();
+		driver.get("http://localhost:" + this.port + "/home");
+		WebElement navNotesTab = driver.findElement(By.id("nav-notes-tab"));
+		navNotesTab.click();
+		Thread.sleep(1000);
+		WebElement editNoteButton1 = driver.findElement(By.id("editNoteButton1"));
+		editNoteButton1.click();
+		Thread.sleep(1000);
+		WebElement noteTitle = driver.findElement(By.name("noteTitle"));
+		WebElement noteDescription = driver.findElement(By.name("noteDescription"));
+		WebElement noteSubmit = driver.findElement(By.id("noteSubmit"));
+		noteTitle.clear();
+		noteDescription.clear();
+		noteTitle.sendKeys("Title 2");
+		noteDescription.sendKeys("Description 2");
+		noteSubmit.submit();
+		Assertions.assertEquals("Result", driver.getTitle());
+		WebElement successMessage = driver.findElement(By.id("success-msg"));
+		Assertions.assertEquals("Your note has been successfully edited.", successMessage.getText());
+		driver.get("http://localhost:" + this.port + "/home");
+		WebElement navNotesTab2 = driver.findElement(By.id("nav-notes-tab"));
+		navNotesTab2.click();
+		Thread.sleep(1000);
+		WebElement noteTitle1 = driver.findElement(By.id("noteTitle1"));
+		WebElement noteDescription1 = driver.findElement(By.id("noteDescription1"));
+		Assertions.assertEquals("Title 2", noteTitle1.getText());
+		Assertions.assertEquals("Description 2", noteDescription1.getText());
 	}
 
 	public void loginUser() {
